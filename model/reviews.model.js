0a8467be18ec69024b4546e60326b60b498012b4
admy0101 <a.d.myers1986@gmail.com>
2022-05-18 11:45:09 +0100

    change to make comment_count a num

--- model/reviews.model.js
+++ model/reviews.model.js
@@ -4,7 +4,7 @@ const reviews = require("../db/data/test-data/reviews.js");
 exports.selectReviewById = (review_id) => {
   return db
     .query(
-      `SELECT reviews.*, COUNT(comments.review_id)::INT AS comment_count FROM reviews
+      `SELECT reviews.*, COUNT(comments.review_id)::INTgit  AS comment_count FROM reviews
       LEFT JOIN comments ON comments.review_id = reviews.review_id WHERE reviews.review_id = $1
       GROUP BY reviews.review_id  `,
       [review_id]
